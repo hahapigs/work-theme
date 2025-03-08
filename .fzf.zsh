773fb2c0d2ea0ee07a1e8f17e58827fa5a830ac1
hahapigs <15711385916@163.com>
2025-03-08 22:15:09 +0800

    feat:把^R、^T、alt-C 和 forgit武装到牙齿

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,5 +1,33 @@
+# For fzf global config
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+--border --padding 1,2
+--input-label ' Input '
+--height 80%                  # Use 80% of terminal height
+--layout reverse              # List appears above the prompt (reverse, up, bottom)
+--border rounded              # Add rounded borders
+--margin 1                    # Set margin to create space around the window
+--info inline                 # Show info inline with results
+--multi                       # Enable multi-selection
+--no-hscroll                  # Disable horizontal scrolling
+--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (exa --tree --level=2 --icons {} || lsd --tree --depth=2 {} || tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+--color 'bg:#1e1e2e,bg+:#313244,fg:#cdd6f4,fg+:#cdd6f4,hl:214,hl+:76'
+--color 'border:#aaaaaa,label:#cccccc,gutter:8'
+--color 'preview-border:#9999cc,preview-label:#ccccff'
+--color 'input-border:#996666,input-label:#ffcccc,prompt:#cba6f7'
+--color 'header:#f38ba8,header-border:#6699cc,header-label:#99ccff'
+--color 'list-border:#669966,list-label:#99cc99'
+--color 'pointer:76,marker:226'
+--pointer='➤ '
+--bind 'result:transform-list-label:[[ -z \$FZF_QUERY ]] && echo \" \$FZF_MATCH_COUNT items \" || echo \" \$FZF_MATCH_COUNT matches for [\$FZF_QUERY] \"'
+--bind 'ctrl-/:toggle-preview'
+--bind 'ctrl-t:top'
+--bind 'ctrl-j:down'
+--bind 'ctrl-k:up'
+--exit-0
+"
+
 # For ctrl-t file search
-export FFZF_CTRL_T_COMMAND="fd --type f
+export FZF_CTRL_T_COMMAND="fd --type f
 --exclude .git
 --exclude node_modules
 --exclude .DS_Store
@@ -17,42 +45,48 @@ fzf_toggle_hidden() {
   fi
 }
 
-# For directory search with alt-c
-export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+# For command search with ctrl-r
+export FZF_CTRL_R_OPTS=$FZF_CTRL_R_OPTS"
+--style full
+--border-label ' 命令查找器 '
+--header-label ' ctrl+y 复制 '
+--prompt='🚀  '
+--bind 'focus:+transform-header:echo -n {2..} || $(echo "No command selected")'
+--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'    # Copy to clipboard
+--bind '>:preview:man \$(echo -n {2..} | awk \"{print \\\$1}\") 2>/dev/null || (echo \"Command error\" | boxes -a c -d tux)'
+--bind '?:preview:timeout 1 tldr \$(echo -n {2..} | awk \"{print \\\$1}\") 2>/dev/null || (echo \"Command error\" | boxes -a c -d cowsay)'
+"
+
 # Custom preview for ctrl-t
 export FZF_CTRL_T_OPTS=$FZF_CTRL_T_OPTS"
---header='$(printf "^. to toggle hidden files")'
---prompt '📁 '
-"
-
-# Custom preview for ctrl-r
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
---height 80%                  # Use 80% of terminal height
---layout=reverse              # List appears above the prompt (reverse, up, bottom)
---border rounded              # Add rounded borders
---margin=1                    # Set margin to create space around the window
---info=inline                 # Show info inline with results
---multi                       # Enable multi-selection
---cycle                       # Ensure cycling through options
---no-hscroll                  # Disable horizontal scrolling
---preview-window=right:60%:wrap    # Preview pane on right, 60% width, wrapped text (up:3:hidden:wrap)
---preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
-# Preview command explanation:
-# - For files: Show with bat (with line numbers) or fall back to cat
-# - For directories: Show tree structure
-# - For other items: Echo the item
---color='bg:#1e1e2e,bg+:#313244,fg:#cdd6f4,fg+:#cdd6f4,hl:214,hl+:76'
---color='header:#f38ba8,info:#cba6f7,prompt:#cba6f7,pointer:76,marker:#f5e0dc,gutter:8'
---header='$(printf "^Y 拷贝到剪切板")'
---prompt=' '
---pointer='➤'
---marker='✓'
---bind 'ctrl-a:select-all'                                      # Select all items
+--style full
+--border-label ' 文件查找器 '
+--header-label ' 文件类型 '
+--prompt '🔍  '
+--marker='✓ '
+--bind 'focus:+transform-header:file --brief {} || $(echo "No file selected")'
+--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'    # Copy to clipboard
 --bind 'ctrl-e:execute(echo {+} | xargs -o vim)'                # Open in vim
 --bind 'ctrl-v:execute(code {+})'                               # Open in VS Code
---bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'    # Copy to clipboard
---bind 'ctrl-/:toggle-preview'                                  # Change preview window
-#--bind '?:change-preview-window(down|hidden|)'
---exit-0
+--bind 'ctrl-a:select-all'                                      # Select all items
 "
 
+# For directory search with alt-c
+# export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+export FZF_ALT_C_OPTS=$FZF_ALT_C_COMMAND"
+--style full
+--border-label ' 目录跳转器 '
+--header-label '  '
+--prompt '📁  '
+--bind 'focus:+transform-header:file --brief {} || $(echo "No file selected")'
+"
+
+# For forgit global config
+export FORGIT_FZF_DEFAULT_OPTS="$FORGIT_FZF_DEFAULT_OPTS
+--style full
+--border-label ' forgit '
+--header-label ' branch '
+--prompt '🛠️  '
+--marker='✓ '
+--bind 'focus:+transform-header:git branch --show-current || $(echo "No git")'
+"
