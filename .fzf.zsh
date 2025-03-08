acb2eee0029ebf51b137b3bb5db29a55c9865960
hahapigs <15711385916@163.com>
2025-03-09 01:42:41 +0800

    refactor:在.fzf.zsh补充Aloxaf/fzf-tab配置，并追加注释

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,3 +1,6 @@
+##########################################################
+##### junegunn/fzf
+##########################################################
 # For fzf global config
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
 --border --padding 1,2
@@ -81,6 +84,9 @@ export FZF_ALT_C_OPTS=$FZF_ALT_C_COMMAND"
 --bind 'focus:+transform-header:file --brief {} || $(echo "No file selected")'
 "
 
+##########################################################
+##### wfxr/forgit  
+##########################################################
 # For forgit global config
 export FORGIT_FZF_DEFAULT_OPTS="$FORGIT_FZF_DEFAULT_OPTS
 --style full
@@ -90,3 +96,29 @@ export FORGIT_FZF_DEFAULT_OPTS="$FORGIT_FZF_DEFAULT_OPTS
 --marker='✓ '
 --bind 'focus:+transform-header:git branch --show-current || $(echo "No git")'
 "
+
+##########################################################
+##### Aloxaf/fzf-tab 
+##########################################################
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# custom fzf flags
+# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+# tmux tmux >= 3.2
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
