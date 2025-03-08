f819c8947f929d9f64ee1ed4bbf8e0558a051934
hahapigs <15711385916@163.com>
2025-03-09 07:00:34 +0800

    vendor:修改mcfly的加载方式

--- zsh/zinit/.zinit.zsh
+++ zsh/zinit/.zinit.zsh
@@ -167,25 +167,6 @@ zinit load wfxr/forgit
 # https://github.com/cantino/mcfly
 zinit ice wait"0b" lucid if"[[ -n '$TMUX' ]] || [[ '$TERM_PROGRAM' != 'WarpTerminal' ]]" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"; bindkey "^R" fzf-history-widget; bindkey "^Y" mcfly-history-widget'
 zinit light cantino/mcfly
-# To swap the color scheme
-[[ "$(defaults read -g AppleInterfaceStyle 2&>/dev/null)" != "Dark" ]] && export MCFLY_LIGHT=true
-export MCFLY_KEY_SCHEME=vim
-# Fuzzy searching, 0 is off, [2-5] is best
-export MCFLY_FUZZY=2
-# Change interface view, options: top、 bottom
-export MCFLY_INTERFACE_VIEW=bottom
-# Data source
-export MCFLY_HISTORY=~/.zsh_history
-# Results count
-export MCFLY_RESULTS=50
-# Results sorting, options: rank、last_run
-export MCFLY_RESULTS_SORT=last_run
-# Custom prompt, default: $, ❯
-export MCFLY_PROMPT=""
-# Search only the latest 10000 entries
-MCFLY_HISTORY_LIMIT=100000
-# Pause used
-# unset MCFLY_HISTORY
 
 ##########################################################
 ##### Load powerlevel10k theme
