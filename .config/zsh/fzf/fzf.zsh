9d3a5bcb46cdeb32237cb4398b0d6bd656b84a2f
hahapigs <15711385916@163.com>
2025-03-13 01:28:06 +0800

    feat:fzf增加对homebrew的支持

--- .config/zsh/fzf/fzf.zsh
+++ .config/zsh/fzf/fzf.zsh
@@ -58,7 +58,6 @@ export FZF_ALT_C_OPTS="
 --border-label ' 目录跳转器 '
 --header-label ' 绝对路径 '
 --prompt '📁  '
---walker-skip .git,node_modules,target
 --bind 'focus:+transform-header:echo \$(pwd | awk \"{print \\\$1}\")/{} || $(echo "No directory selected")'
 "
 
@@ -180,4 +179,81 @@ fkill() {
   then
     echo $pid | xargs kill -${1:-9}
   fi
+}
+
+########################
+## Homebrew
+########################
+# Install (one or multiple) selected application(s)
+# using "brew search" as source input
+# mnemonic [B]rew [I]nstall [P]ackage
+bip() {
+  local inst=$(brew search "$@" | fzf -m)
+
+  if [[ $inst ]]; then
+    for prog in $(echo $inst);
+    do; brew install $prog; done;
+  fi
+}
+
+# Update (one or multiple) selected application(s)
+# mnemonic [B]rew [U]pdate [P]ackage
+bup() {
+  local upd=$(brew leaves | fzf -m)
+
+  if [[ $upd ]]; then
+    for prog in $(echo $upd);
+    do; brew upgrade $prog; done;
+  fi
+}
+
+# Delete (one or multiple) selected application(s)
+# mnemonic [B]rew [C]lean [P]ackage (e.g. uninstall)
+bcp() {
+  local uninst=$(brew leaves | fzf -m)
+
+  if [[ $uninst ]]; then
+    for prog in $(echo $uninst);
+    do; brew uninstall $prog; done;
+  fi
+}
+
+# Install or open the webpage for the selected application 
+# using brew cask search as input source
+# and display a info quickview window for the currently marked application 
+bicp() {
+    local token
+    token=$(brew search --casks "$1" | fzf --preview 'brew info {}')
+                    
+    if [ "x$token" != "x" ]                                                                
+    then             
+        echo "(I)nstall or open the (h)omepage of $token"
+        read input                             
+        if [ $input = "i" ] || [ $input = "I" ]; then    
+            brew install --cask $token                   
+        fi                                                                                    
+        if [ $input = "h" ] || [ $input = "H" ]; then                                         
+            brew home $token                     
+        fi                                           
+    fi                             
+}
+
+# Uninstall or open the webpage for the selected application
+# using brew list as input source (all brew cask installed applications)
+# and display a info quickview window for the currently marked application
+bccp() {
+    local token
+    token=$(brew list --casks | fzf --preview 'brew info {}')
+
+    if [ "x$token" != "x" ]
+    then
+        echo "(U)ninstall or open the (h)omepae of $token"
+        read input
+        if [ $input = "u" ] || [ $input = "U" ]; then
+            brew uninstall --cask $token
+        fi
+        if [ $input = "h" ] || [ $token = "h" ]; then
+            brew home $token
+        fi
+    fi
 }
