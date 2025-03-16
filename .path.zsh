018120c9eeee1828b39786cf80008443f9c42638
hahapigs <15711385916@163.com>
2025-03-16 14:24:46 +0800

    ✨ feat: 支持PATH加载自定义command

--- .path.zsh
+++ .path.zsh
@@ -1,44 +1,42 @@
-#!/bin/zsh
 # Note: The first added entry gets referenced last
 if command -v getconf &> /dev/null; then
   PATH="$(getconf PATH)"
 fi
 
 # Prepend PATH by skipping duplicates
-declare -U PATH
+declare -U PATH PATH
 prepend() {
   [ -d "$1" ] && PATH="$1:$PATH"
 }
 
-# Obtain the Homebrew prefix using `BREW_PREFIX`  based on the operating system
+# Obtain the Homebrew prefix using `brewprefix`  based on the operating system
 if [[ "$(uname -s)" == "Darwin" ]]; then
   if [[ "$(uname -m)" == "arm64" ]]; then
-    BREW_PREFIX="/opt/homebrew"  # Apple Silicon
+    brewprefix="/opt/homebrew"  # Apple Silicon
   else
-    BREW_PREFIX="/usr/local"     # Intel
+    brewprefix="/usr/local"     # Intel
   fi
 elif [[ "$(uname -s)" == "Linux" ]]; then
-  BREW_PREFIX="/home/linuxbrew/.linuxbrew"  # Linux
+  brewprefix="/home/linuxbrew/.linuxbrew"  # Linux
 else
-  echo "The BREW_PREFIX environment variable is incorrect."
+  echo "The brew-prefix environment variable is incorrect."
 fi
 
+# Homebrew
 # > $(brew --prefix)
-prepend "$BREW_PREFIX/bin"
-prepend "$BREW_PREFIX/sbin"
+prepend "$brewprefix/bin"
+prepend "$brewprefix/sbin"
 
 # Jenv
 prepend "$HOME/.jenv/bin"
 eval "$(jenv init -)"
 
 # Custom dotfiles binaries
-prepend "$/dotfiles/bin/lib"
-prepend "$HOME/dotfiles/bin"
-
-# User binaries
-prepend "$HOME/bin"
+prepend "$HOME/.config/zsh/bin"
 
 # Prevent it from being used accidentally elsewhere in the script or by other scripts
+unset brewprefix
 unset prepend
 
 export PATH
+export FPATH
