4116540a91397b49bd487d1f581d7f1f4c49c6fd
hahapigs <15711385916@163.com>
2025-03-28 13:40:10 +0800

    ♻️  refactor: Refactor the code for install script

--- cellar_install.sh
+++ cellar_install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-set -e # 脚本执行出错时自动退出
 
-# ========== 目录跳转和显示 ==========
-# lsd (colorls在mac上安装需要先安装ruby，推荐安装lsd)
-brew install lsd
-# exa
-brew install exa
-# broot
-brew install broot
+# brew install lsd
 
+brew install eza
 
-# ========== 网络管理 ==========
+# brew install broot
+
 brew install curl
 
 brew install curlie
@@ -22,10 +17,9 @@ brew install httpie
 brew install mtr
 
 brew install telnet
+
 # 基于 traceroute 和 tracepath 命令
 brew install nexttrace
-
-# ========== 磁盘线程性能显示 ==========
 
 brew install dust
 
@@ -41,8 +35,19 @@ brew install htop
 
 brew install glances
 
+brew install neofetch
 
-# ========== 查找工具 ==========
+# brew install fastfetch
+
+# brew install screenfetch
+
+brew install gh
+
+brew install gitmoji
+
+brew install commitizen
+
+brew install commitlint 
 
 brew install fzf
 
@@ -52,9 +57,14 @@ brew install mcfly
 # choose  choose-gui  choose-rust
 brew install choose
 
-
 # diff 的替代品
 brew install diff-so-fancy
 
 # atuin 历史记录管理工具，支持多端同步
 brew install atuin
+
+brew install jenv
+
+brew install direnv
+
+brew install mise
