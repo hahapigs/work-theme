d19707f329ce19363662078ec716af25b1bc2222
hahapigs <15711385916@163.com>
2025-04-06 20:30:50 +0800

    ♻️  refactor: Refactor the code in cellar_install.sh

--- cellar_install.sh
+++ cellar_install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# brew install lsd
-
+############ 目录跳转和显示 ##########
+# lsd
+brew install lsd
+# eza
 brew install eza
+# broot
+brew install broot
+# ranger
+brew install ranger
+# 目录跳转
+# brew install autojump
+# brew install z
+brew install zoxide
 
-# brew install broot
-
+################################
+## 网络管理
+################################
 brew install curl
 
 brew install curlie
@@ -21,8 +32,16 @@ brew install telnet
 # 基于 traceroute 和 tracepath 命令
 brew install nexttrace
 
+################################
+## 硬件性能监控
+################################
+# du 命令的替代品，显示每个目录和文件占用大小
 brew install dust
 
+# du 命令的替代品
+# brew install ncdu
+
+# df 命令的替代品，查看磁盘占用情况
 brew install duf
 
 brew install procs
@@ -34,25 +53,21 @@ brew install btop
 brew install htop
 
 brew install glances
-
-brew install neofetch
+# sampler --config sampler.yml
+brew install sampler
 
 # brew install fastfetch
-
 # brew install screenfetch
+brew install neofetch
 
-brew install gh
-
-brew install gitmoji
-
-brew install commitizen
-
-brew install commitlint 
-
+################################
+## 命令行查找工具
+################################
 brew install fzf
 
 brew install mcfly
 
+brew install mcfly
 
 # choose  choose-gui  choose-rust
 brew install choose
@@ -68,3 +83,14 @@ brew install jenv
 brew install direnv
 
 brew install mise
+
+brew install tmux
+
+brew install gh
+
+brew install gitmoji
+
+brew install commitizen
+
+brew install commitlint 
+
