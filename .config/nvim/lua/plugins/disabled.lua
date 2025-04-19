4416efcee1cff9674226fd2ae29886c98caf55d7
hahapigs <15711385916@163.com>
2025-04-19 22:29:30 +0800

    ✨ feat: neovim 使用自定义dashboard

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
+  -- { "goolord/alpha-nvim", enabled = false },
+  -- { "echasnovski/mini.starter", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
+}
